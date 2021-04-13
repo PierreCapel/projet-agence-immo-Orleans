40bd1309974b87b5821ec13c6188f79e18099c4e
PierreCapel <pecapel@gmail.com>
2021-04-13 12:06:35 +0200

    cleared AnnonceController and AnnonceManager per PR comment

--- src/Model/AnnonceManager.php
+++ src/Model/AnnonceManager.php
@@ -2,31 +2,6 @@
 
 namespace App\Model;
 
-// class AnnonceManager extends AbstractManager
-// {
-//     public const TABLE = 'item';
-
-//     /**
-//      * Insert new item in database TODO
-//      */
-//     public function insert(array $item): int
-//     {
-//         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`) VALUES (:title)");
-//         $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-
-//         $statement->execute();
-//         return (int)$this->pdo->lastInsertId();
-//     }
-
-//     /**
-//      * Update item in database TODO
-//      */
-//     public function update(array $item): bool
-//     {
-//         $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-//         $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
-//         $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-
-//         return $statement->execute();
-//     }
-// }
+class AnnonceManager extends AbstractManager
+{
+}
