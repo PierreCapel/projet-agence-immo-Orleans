2c5a86087abc2f1d9d5ce2bb080f5fd3c5dcc075
PierreCapel <pecapel@gmail.com>
2021-04-20 19:57:21 +0200

    for master rebase

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -21,26 +21,57 @@ class AdminController extends AbstractController
      */
     public function loggin()
     {
-        return $this->twig->render('Admin/loggin.html.twig');
+        return $this->twig->render('admin/loggin.html.twig');
     }
     public function index()
     {
-        return $this->twig->render('Admin/index.html.twig');
+        return $this->twig->render('admin/index.html.twig');
     }
     public function modifAnnonce()
     {
-        return $this->twig->render('Admin/modifAnnonce.html.twig');
+        return $this->twig->render('admin/modifannonce.html.twig');
     }
     public function ajoutAnnonce()
     {
-        return $this->twig->render('Admin/ajoutAnnonce.html.twig');
+        return $this->twig->render('admin/ajoutannonce.html.twig');
     }
     public function modifSlogan()
     {
-        return $this->twig->render('Admin/modifSlogan.html.twig');
+        return $this->twig->render('admin/modifslogan.html.twig');
     }
     public function modifDocument()
     {
-        return $this->twig->render('Admin/modifDocument.html.twig');
+        return $this->twig->render('admin/modifdocument.html.twig');
+    }
+    public function ajoutPhoto()
+    {
+        return $this->twig->render('admin/ajoutphoto.html.twig');
+    }
+
+
+    //fonction d'ajout des images par formulaire
+    public function pictureUpload()
+    {
+        //check methode serveur
+        if ($_SERVER["REQUEST_METHOD"] === "POST" && (!empty($_POST['pictureUpload']))) 
+        {
+            //set dossier reception
+            $uploadDir = __DIR__ . "/../../public/assets/images/annonces/";
+            //recup extension fichier
+            $extension = pathinfo($_FILES['annoncePicture']['name'], PATHINFO_EXTENSION);
+            //set chemin fichier
+            $uploadFile = $uploadDir . basename($_FILES['annoncePicture']['name']);
+            //set liste d'extensions
+            $extensionsOk = ['jpg', 'jpeg', 'png'];
+            //check extension du fichier vs extensions autorisées
+            if (!in_array($extension, $extensionsOk)) 
+            {
+                echo 'L\'image doit etre de type jpeg, jpg ou png.';
+            } 
+            else 
+            {
+                move_uploaded_file($_FILES['annoncePicture']['tmp_name'], $uploadFile);
+            }
+        }
     }
 }
