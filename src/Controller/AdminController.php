d77cfe902207242cb9944bdda4a65121eb3deaa9
PierreCapel <pecapel@gmail.com>
2021-04-21 11:36:44 +0200

    first try

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -45,26 +45,29 @@ class AdminController extends AbstractController
     }
     public function ajoutPhoto()
     {
+        var_dump($_POST);
+        $this->upload();
         return $this->twig->render('admin/ajoutphoto.html.twig');
     }
-    //fonction d'ajout des images par formulaire
-    public function pictureUpload()
+    //fonction d'ajout des images par formulaire 
+    private function upload()
     {
         //check methode serveur
-        if ($_SERVER["REQUEST_METHOD"] === "POST" && (!empty($_POST['pictureUpload']))) {
+        if ($_SERVER["REQUEST_METHOD"] === "POST" && (!empty($_FILES))) {
             //set dossier reception
-            $uploadDir = __DIR__ . "/../../public/assets/images/annonces/";
+            $uploadDir = __DIR__ . "/";
             //recup extension fichier
-            $extension = pathinfo($_FILES['annoncePicture']['name'], PATHINFO_EXTENSION);
-            //set chemin fichier
-            $uploadFile = $uploadDir . basename($_FILES['annoncePicture']['name']);
+            $extension = pathinfo($_FILES['pictureUpload']['name'], PATHINFO_EXTENSION);
+            //set chemin destination fichier
+            $uploadFile = $uploadDir . basename($_FILES['pictureUpload']['name']);
             //set liste d'extensions
             $extensionsOk = ['jpg', 'jpeg', 'png'];
             //check extension du fichier vs extensions autorisées
             if (!in_array($extension, $extensionsOk)) {
                 echo 'L\'image doit etre de type jpeg, jpg ou png.';
             } else {
-                move_uploaded_file($_FILES['annoncePicture']['tmp_name'], $uploadFile);
+                move_uploaded_file($_FILES['pictureUpload']['tmp_name'], $uploadFile);
+                echo "file uploaded";
             }
         }
     }
