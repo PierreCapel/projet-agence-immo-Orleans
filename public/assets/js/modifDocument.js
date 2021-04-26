3bf20d6d6c2935e2d98fe90eee36275089ef22ac
Patrick VONGSA <patrick.vongsa@gmail.com>
2021-04-26 10:44:52 +0200

    access and modify database ok, html and layout done

--- public/assets/js/modifDocument.js
+++ public/assets/js/modifDocument.js
@@ -6,36 +6,25 @@ const form = document.getElementById('monform');
 let pTag = document.getElementsByClassName('mabalise');
 let suppButton = document.getElementsByClassName('supp');
 
-function addListenerDelete() {
-    if (suppButton.length != 0) {
-    for (let i = 0; i < suppButton.length; i++) {
-        suppButton[i].addEventListener('click', (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            console.log(e)
-            suppButton[i].parentNode.parentNode.removeChild(pTag[i]);
-            pTag = document.getElementsByClassName('mabalise');
-            suppButton = document.getElementsByClassName('supp');
-        }
-        )
-    }}};
-
-    addListenerDelete();
+let id = pTag.length;
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
     //création balise p
     const maNouvelleBalise = document.createElement('p');
     maNouvelleBalise.classList.add('mabalise');
+    maNouvelleBalise.classList.add('d-flex');
+    maNouvelleBalise.classList.add('align-items-center');
 
-    let newId = pTag.length + 1;
 
     //création balise input
-    const newInput = document.createElement('input');
-    newInput.type = "text";
-    newInput.name = 'justificatifs' + newId;
-    newInput.id = newId;
-    newInput.classList.add('input-doc');
+    id += 1;
+    const newTextArea = document.createElement('textarea');
+    newTextArea.type = "text";
+    newTextArea.name = 'justificatifs' + id;
+    newTextArea.id = id;
+    newTextArea.rows = "1";
+    newTextArea.classList.add('textarea-doc');
 
     //création balise a
     const newLink = document.createElement('a');
@@ -46,15 +35,30 @@ button.addEventListener('click', (e) => {
     newLink.innerHTML = "Supprimer";
 
     //ajout balises input et a dans p
-    maNouvelleBalise.appendChild(newInput);
+    maNouvelleBalise.appendChild(newTextArea);
     maNouvelleBalise.appendChild(newLink);
 
     //ajout p dans form 
     form.appendChild(maNouvelleBalise);
-    pTag = document.getElementsByClassName('mabalise');
-    suppButton = document.getElementsByClassName('supp');
-    addListenerDelete();
+
+
+    newLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(e)
+        newLink.parentNode.parentNode.removeChild(maNouvelleBalise);
+    }
+    )
+    
 });
 
-
+for (let i = 0; i < suppButton.length; i++) {
+    suppButton[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(e)
+        suppButton[i].parentNode.parentNode.removeChild(pTag[i]);
+    }
+    )
+};
 
--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -29,4 +29,28 @@ class AdminManager extends AbstractManager
 
         return $statement->execute();
     }
+
+    public function getListDoc()
+    {
+        $query = 'SELECT * FROM justificatifs';
+        $statement = $this->pdo->query($query);
+        $list = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $list;
+    }
+
+    public function modifyListDoc(array $listDoc)
+    {
+        //vide la table justificatifs
+        $query = 'TRUNCATE TABLE `justificatifs`';
+        $statement = $this->pdo->query($query);
+
+        //ajout de la nouvelle liste
+        foreach ($listDoc as $field => $value) {
+            $query = 'INSERT INTO justificatifs(justificatifs) VALUES (:' . $field . ')';
+            $statement = $this->pdo->prepare($query);
+            $statement->bindValue(":$field", $value, PDO::PARAM_STR);
+            $statement->execute();
+        }
+    }
 }
